36faeaae85f974fdb45dba8e7229c342b9c9c04f
Kirill Radaev <cyrilrdv@gmail.com>
2019-10-30 17:15:34 -0700

    bricks, background, some animations

--- public/assets/js/SceneMain.js
+++ public/assets/js/SceneMain.js
@@ -2,6 +2,7 @@ class SceneMain extends Phaser.Scene {
   constructor() {
     super({ key: "SceneMain" });
   }
+
   preload() {
     this.load.image("sprPlayer", "assets/content/sprPlayer.png");
     this.load.image("brickYellow", "assets/content/brickYellow.png");
@@ -10,21 +11,19 @@ class SceneMain extends Phaser.Scene {
     this.load.image("brickPurple", "assets/content/brickPurple.png");
     this.load.image("platform", "assets/content/platform.png");
   }
+
   create() {
-    // this.anims.create({
-    //   key: "sprPlayer",
-    //   frames: this.anims.generateFrameNumbers("sprPlayer"),
-    //   frameRate: 20,
-    //   repeat: -1
-    // });
-    // this.platform = this.physics.add.static("platform");
-    let player = this.physics.add.image(
-      config.width * 0.5,
-      config.height * 0.9,
-      "sprPlayer"
-    );
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
+    //Player creation
+    // this.players = this.physics.add.group();
+
+    // let player = this.physics.add.image(
+    //   config.width * 0.5,
+    //   config.height * 0.9,
+    //   "sprPlayer"
+    // );
+
+    // this.players.add(player);
+
     this.background = this.add.tileSprite(
       0,
       0,
@@ -32,28 +31,59 @@ class SceneMain extends Phaser.Scene {
       config.height,
       "background"
     );
+
     this.background.setOrigin(0, 0);
+
     this.brick1 = this.add.image(400, 0, "brickYellow");
     this.brick2 = this.add.image(650, 0, "brickGreen");
     this.brick3 = this.add.image(400, 0, "brickBlue");
     this.brick4 = this.add.image(650, 0, "brickPurple");
+
     //  The score
     let score = 0;
     this.scoreText = this.add.text(16, 16, "SCORE: " + score, {
       fontSize: "32px",
       fill: "#DC143C"
     });
+
     this.scoreText;
+
     this.player = new Player(
       this,
       this.game.config.width * 0.5,
-      this.game.config.height * 0.98,
+      this.game.config.height * 0.83,
       "sprPlayer"
     );
-    console.log(this.player);
+
     this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+    //Platform creation
+
+    this.physics.world.setBoundsCollision();
+
+    // let platfroms = this.physics.add.staticGroup();
+
+    // platforms
+    //   .create(this.game.width * 0.5, this.game.height * 0.9, "platform")
+    //   .setScale(2)
+    //   .refreshBody();
+
+    // this.platforms = this.physics.add.group();
+
+    // let platform = this.physics.add.image(
+    //   this.game.config.width * 0.5,
+    //   this.game.config.height * 0.95,
+    //   "platform"
+    // );
+
+    // this.platforms.add(platform);
+
+    // platform.body.allowGravity = false;
+
+    // this.physics.add.collider(platform, player);
   }
+
   update() {
     //Check's if the player if alive, if not it will show the game over screen
     if (!this.player.getData("isDead")) {
@@ -64,8 +94,11 @@ class SceneMain extends Phaser.Scene {
         this.player.moveRight();
       }
     }
+
     this.score += 10;
+
     this.moveBrick1(this.brick1, 3);
+
     this.time.addEvent({
       delay: 1000,
       callback: function() {
@@ -74,6 +107,7 @@ class SceneMain extends Phaser.Scene {
       callbackScope: this,
       loop: false
     });
+
     this.time.addEvent({
       delay: 2000,
       callback: function() {
@@ -82,6 +116,7 @@ class SceneMain extends Phaser.Scene {
       callbackScope: this,
       loop: false
     });
+
     this.time.addEvent({
       delay: 3000,
       callback: function() {
@@ -90,6 +125,7 @@ class SceneMain extends Phaser.Scene {
       callbackScope: this,
       loop: false
     });
+
     this.time.addEvent({
       delay: 2000,
       callback: function() {
@@ -98,8 +134,10 @@ class SceneMain extends Phaser.Scene {
       callbackScope: this,
       loop: false
     });
+
     this.background.tilePositionY -= 1.5;
   }
+
   //Sets brick's speed
   moveBrick1(brick, speed) {
     brick.y += speed;
@@ -107,21 +145,25 @@ class SceneMain extends Phaser.Scene {
       this.resetBrickPosition1(brick);
     }
   }
+
   moveBrick2(brick, speed) {
     brick.y += speed;
     if (brick.y > config.height) {
       this.resetBrickPosition2(brick);
     }
   }
+
   //Resets brick's position after it reaches the end of a screen
   resetBrickPosition1(brick) {
     brick.y = 0;
     brick.x = 400;
   }
+
   resetBrickPosition2(brick) {
     brick.y = 0;
     brick.x = 650;
   }
+
   increaseScore(item) {
     item += 10;
   }
